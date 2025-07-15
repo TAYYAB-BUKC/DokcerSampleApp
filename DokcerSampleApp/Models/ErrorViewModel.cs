ea37a3b96a67f65c5724d6ddbf9425b9798ad67d
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-15 23:33:34 +0500

    Add project files.

---
+++ DokcerSampleApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DokcerSampleApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
